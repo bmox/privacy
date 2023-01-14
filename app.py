f493a563e621ead1c5dc2ea37c9edc2f0b654279
bmox <62886280+bmox@users.noreply.github.com>
2023-01-14 17:29:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-import gradio as gr
-import numpy as np
+from streamlit_webrtc import webrtc_streamer
 
-def flip(im):
-    return np.flipud(im)
-
-demo = gr.Interface(
-    flip, 
-    gr.Image(source="webcam", streaming=True), 
-    "image",
-    live=True
-)
-if __name__ == "__main__":
-    demo.launch()
+webrtc_streamer(key="sample")
